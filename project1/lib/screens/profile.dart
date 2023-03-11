e27ef8eed0dd971bf5cea354fec6385dc1a64a30
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-11 18:07:16 +0200

    creating a profile file screen

---
+++ project1/lib/screens/profile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatefulWidget {
+  const Profile({super.key});
+
+  @override
+  State<Profile> createState() => _HomeState();
+}
+
+class _HomeState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile")),
+    );
+  }
+}
