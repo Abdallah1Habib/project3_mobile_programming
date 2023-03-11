6063a411fa836fb42d25c44e502273838406df1a
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-11 17:36:33 +0200

    creating responsive file to show to user

--- project1/lib/views/mobile.dart
+++ project1/lib/views/mobile.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class Mobilescreen extends StatefulWidget {
+  const Mobilescreen({super.key});
+
+  @override
+  State<Mobilescreen> createState() => _MobilescreenState();
+}
+
+class _MobilescreenState extends State<Mobilescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mobile screen"),
+      ),
+    );
+  }
+}
