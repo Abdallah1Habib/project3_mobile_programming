9fe628062ac5736d6ac0fae8e4716a1edd2a09bf
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-11 17:59:51 +0200

    creating a constants file that holds all colors of the app

---
+++ project1/lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const mobcolor = Color.fromARGB(0, 0, 0, 1);
+const webcolor = Color.fromARGB(32, 33, 36, 1);
+const primarycolor = Colors.white;
+const secondarycolor = Color.fromARGB(166, 158, 158, 158);
+const bluecolor = Color.fromARGB(0, 149, 246, 1);
