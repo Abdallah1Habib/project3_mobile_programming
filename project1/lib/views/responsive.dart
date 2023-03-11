95d8ab69a18d71e9c8c838ab9ae6531c0bbbbb10
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-11 17:49:19 +0200

    cleaning the responsive file code

--- project1/lib/views/responsive.dart
+++ project1/lib/views/responsive.dart
@@ -1,5 +1,6 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, avoid_types_as_parameter_names, non_constant_identifier_names
+
 import 'package:flutter/material.dart';
-import 'package:project1/views/web.dart';
 
 class Responsive extends StatefulWidget {
   final mobile;
