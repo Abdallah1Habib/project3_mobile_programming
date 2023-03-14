16cdcbf07fc098748e158750e388f357e7440f39
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-14 17:24:14 +0200

    making the home body color main color black

--- project1/lib/screens/home.dart
+++ project1/lib/screens/home.dart
@@ -12,6 +12,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: mobcolor,
       appBar: AppBar(
         backgroundColor: mobcolor,
         title: Image.asset(
