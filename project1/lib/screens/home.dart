d08db57ad752f629ffdb01a458cb1b4a654ae1f1
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-14 17:17:36 +0200

    adding a picture as the title for the home page

--- project1/lib/screens/home.dart
+++ project1/lib/screens/home.dart
@@ -11,7 +11,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home")),
+      appBar: AppBar(
+        title: Image.asset("name"),
+      ),
     );
   }
 }
