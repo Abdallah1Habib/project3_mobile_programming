7be2deb73e44f65f074263caefd2cbd57e3a45c3
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-15 21:54:24 +0200

    adding margin spaces in the home page from the screen

--- project1/lib/screens/home.dart
+++ project1/lib/screens/home.dart
@@ -31,110 +31,113 @@ class _HomeState extends State<Home> {
                 IconButton(onPressed: () {}, icon: Icon(Icons.logout))
               ],
             ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 13),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 16, horizontal: 13),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 13),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(3),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: secondarycolor,
+                        ),
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: NetworkImage("url"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 17,
+                      ),
+                      Text(
+                        "Abdallah habib",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+                ],
+              ),
+            ),
+            Image.network(
+              "src",
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 14),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                          onPressed: () {}, icon: Icon(Icons.favorite_border)),
+                      IconButton(
+                          onPressed: () {}, icon: Icon(Icons.comment_outlined)),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.send),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.bookmark_outline)),
+                ],
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "10 likes",
+                  style: TextStyle(fontSize: 16),
+                )),
+            Row(
               children: [
-                Row(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(3),
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: secondarycolor,
-                      ),
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: NetworkImage("url"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 17,
-                    ),
-                    Text(
-                      "Abdallah habib",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  ],
+                Text(
+                  "Username",
+                  style: TextStyle(fontSize: 17, color: primarycolor),
                 ),
-                IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+                Text(
+                  "Image description",
+                  style: TextStyle(fontSize: 15, color: primarycolor),
+                ),
               ],
             ),
-          ),
-          Image.network(
-            "src",
-            fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height * 0.3,
-            width: MediaQuery.of(context).size.width,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 14),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.favorite_border)),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.comment_outlined)),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.send),
-                    ),
-                  ],
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                margin: EdgeInsets.fromLTRB(10, 9, 9, 10),
+                width: MediaQuery.of(context).size.width,
+                child: Text(
+                  "view all 100 comments",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
-                IconButton(
-                    onPressed: () {}, icon: Icon(Icons.bookmark_outline)),
-              ],
+              ),
             ),
-          ),
-          Container(
-              margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 0, 9, 10),
               width: MediaQuery.of(context).size.width,
               child: Text(
-                "10 likes",
-                style: TextStyle(fontSize: 16),
-              )),
-          Row(
-            children: [
-              Text(
-                "Username",
-                style: TextStyle(fontSize: 17, color: primarycolor),
-              ),
-              Text(
-                "Image description",
-                style: TextStyle(fontSize: 15, color: primarycolor),
-              ),
-            ],
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              margin: EdgeInsets.fromLTRB(10, 9, 9, 10),
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                "view all 100 comments",
+                "14/3/2023",
                 style: TextStyle(
                   fontSize: 16,
                 ),
               ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(10, 0, 9, 10),
-            width: MediaQuery.of(context).size.width,
-            child: Text(
-              "14/3/2023",
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
