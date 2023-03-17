c65102aab902b985fac676188526e5a254eba542
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-17 17:49:32 +0200

    changing the app bar color in the register and login to black

--- project1/lib/screens/constants.dart
+++ project1/lib/screens/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const paybuttoncolor = Color.fromARGB(255, 241, 39, 100);
 const mainbuttoncolor = Color.fromARGB(255, 73, 179, 105);
-const appbarcolor = Color.fromARGB(255, 76, 141, 95);
+const appbarcolor = Colors.black;
