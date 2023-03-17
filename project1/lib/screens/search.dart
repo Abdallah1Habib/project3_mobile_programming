3ddbed4fd86411408df2c3cb5d7b7ac11bff891e
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-17 17:21:32 +0200

    changing the app bar and page color

--- project1/lib/screens/search.dart
+++ project1/lib/screens/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:project1/constants/colors.dart';
 
 class Search extends StatefulWidget {
   const Search({super.key});
@@ -11,10 +12,13 @@ class _HomeState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: mobcolor,
       appBar: AppBar(
+          backgroundColor: mobcolor,
           title: TextFormField(
-        decoration: const InputDecoration(labelText: "Search for a user..."),
-      )),
+            decoration:
+                const InputDecoration(labelText: "Search for a user..."),
+          )),
     );
   }
 }
