773e70f23705072098509ef70ff09867b1c8efb2
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-17 16:41:13 +0200

    adding space between all the items of the row

--- project1/lib/screens/profile.dart
+++ project1/lib/screens/profile.dart
@@ -19,6 +19,7 @@ class _HomeState extends State<Profile> {
       body: Column(
         children: [
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
                 padding: EdgeInsets.all(3),
