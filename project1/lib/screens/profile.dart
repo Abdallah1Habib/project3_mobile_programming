4eee0fc6ab74a95c42e3cc01bc07e73713c3ac87
Abdallah1Habib <abdullah.muhammad.200185@btu.edu.eg>
2023-03-17 17:04:07 +0200

    centring the two button in the row

--- project1/lib/screens/profile.dart
+++ project1/lib/screens/profile.dart
@@ -98,6 +98,7 @@ class _HomeState extends State<Profile> {
             thickness: 0.07,
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               ElevatedButton.icon(
                 onPressed: () {},
